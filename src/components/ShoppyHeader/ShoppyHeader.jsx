c86e336fb1625265a18c9bd872b8495c915e7153
it1403 <anko@gsitm.com>
2024-01-18 09:48:30 +0900

    firebase admin setting

--- src/components/ShoppyHeader/ShoppyHeader.jsx
+++ src/components/ShoppyHeader/ShoppyHeader.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaShoppingBag } from "react-icons/fa";
 import { BsFillPencilFill } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../../api/firebase";
+import User from "../User/User";
 
 export default function ShoppyHeader() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    // onUserStateChange(setUser);
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    });
+  }, []);
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-4xl text-brand">
@@ -16,7 +27,9 @@ export default function ShoppyHeader() {
         <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill />
         </Link>
-        <button>Login</button>
+        {user && <User user={user} />}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
       </nav>
     </header>
   );
