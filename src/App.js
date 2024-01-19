8a18f536904f2109f0047f3ffaac8f2ba67dd490
김명욱 <anko@gsitm.com>
2024-01-19 13:11:34 +0900

    cloudinary image upload 연동

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import ShoppyHeader from "./components/ShoppyHeader/ShoppyHeader";
+import { AuthContextProvider } from "./components/context/AuthContext";
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <ShoppyHeader />
       <Outlet />
-    </>
+    </AuthContextProvider>
   );
 }
 
--- src/components/ShoppyHeader/ShoppyHeader.jsx
+++ src/components/ShoppyHeader/ShoppyHeader.jsx
@@ -1,20 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { FaShoppingBag } from "react-icons/fa";
 import { BsFillPencilFill } from "react-icons/bs";
-import { login, logout, onUserStateChange } from "../../api/firebase";
 import User from "../User/User";
+import Button from "../ui/Button";
+import { useAuthContext } from "../context/AuthContext";
 
 export default function ShoppyHeader() {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    // onUserStateChange(setUser);
-    onUserStateChange((user) => {
-      console.log(user);
-      setUser(user);
-    });
-  }, []);
+  const { user, login, logout } = useAuthContext();
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-4xl text-brand">
@@ -23,13 +16,16 @@ export default function ShoppyHeader() {
       </Link>
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
-        <Link to="/carts">Carts</Link>
-        <Link to="/products/new" className="text-2xl">
-          <BsFillPencilFill />
-        </Link>
+        {user && <Link to="/carts">Carts</Link>}
+        {user && user.isAdmin && (
+          <Link to="/products/new" className="text-2xl">
+            <BsFillPencilFill />
+          </Link>
+        )}
+
         {user && <User user={user} />}
-        {!user && <button onClick={login}>Login</button>}
-        {user && <button onClick={logout}>Logout</button>}
+        {!user && <Button text={"Login"} onClick={login} />}
+        {user && <Button text={"Logout"} onClick={logout} />}
       </nav>
     </header>
   );
