81ce63fa6157c71eeac68f84c4a5305c250fb7d2
it1403 <anko@gsitm.com>
2024-01-22 15:04:39 +0900

    제품 상세 페이지

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 export default function Banner() {
   return (
-    <div>
-      Banner
+    <section className='h-96 bg-yellow-900 relative'>
+    <div className='w-full h-full bg-cover bg-banner bg-center opacity-80'></div>
+    <div className='absolute w-full top-32 text-center text-gray-50 drop-shadow-2xl'>
+      <h2 className='text-6xl'>Shop With KOR</h2>
+      <p className='text-2xl'>Best Products, High Quality</p>
     </div>
+
+  </section>
   );
 }
 
